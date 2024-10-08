c96cf3b09f3b87c83fc5dd945b6e3b9227712ebc
rf-santana <rafasune@outlook.com>
2024-10-07 22:01:37 -0300

    1

---
+++ aula_07-10-Matriz/media.js
@@ -0,0 +1,7 @@
+var notaUm = parseFloat(prompt("Insira a nota: "));
+var notaDois = parseFloat(prompt("Insira a nota: "));
+var notaTres = parseFloat(prompt("Insira a nota: "));
+var notaQuat = parseFloat(prompt("Insira a nota: "));
+
+media = (notaUm + notaDois + notaTres + notaQuat) / 4
+console.log("A media final é: " + media) 
