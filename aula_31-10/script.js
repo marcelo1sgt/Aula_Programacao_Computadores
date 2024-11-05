ca4dba5487be05528d1b3a3f82e35e43001e3ebb
rf-santana <rafasune@outlook.com>
2024-11-04 21:45:16 -0300

    0

--- aula_31-10/script.js
+++ aula_31-10/script.js
@@ -2,12 +2,11 @@
 Elabore um programa que receba um numero e exiba sua tabuada usando o while */
 
 
-var numero = parseInt(prompt("Digite um número para ver sua tabuada: "));
-var contador = 1;
-while (contador <= 10) {
-    var resultado = numero * contador;
-    console.log(numero + " x " + contador + " = " + resultado);
-    contador++;
+var numero = parseFloat(prompt("Digite um número para ver sua tabuada: "));
+var i = 1;
+while (i < 11) {
+    console.log(numero + " x " + i +  " = " + i * numero);
+    i++;
 }
 
 
